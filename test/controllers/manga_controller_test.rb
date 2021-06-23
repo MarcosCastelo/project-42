b19323ce39dbf4ab7414f2b22975c38f4e658552
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-06-23 17:55:20 -0300

    feat: Add test for response of 'mangas' and 'mangas/:id'

--- test/controllers/manga_controller_test.rb
+++ test/controllers/manga_controller_test.rb
@@ -9,10 +9,16 @@ class MangaControllerTest < ActionDispatch::IntegrationTest
   test "should get manga list" do
     get '/mangas'
     assert_response :success
+
+    Manga.all.each do |manga|
+      assert_select "li", manga.title
+    end
   end
 
   test "should get manga detail" do
     get "/mangas/#{@manga.id}"
     assert_response :success
+    assert_select "h1", @manga.title
+    assert_select "p", @manga.link
   end
 end
