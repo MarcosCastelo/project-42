ae3b92d1f37aa26d4b2f7cc443efa4c68dd80d3b
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-06-23 17:18:18 -0300

    feat: Test the routes 'mangas' and 'mangas/:id'

--- test/controllers/manga_controller_test.rb
+++ test/controllers/manga_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class MangaControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @manga = Manga.new(title: 'One Piece', link: 'https://mangafox.la')
+    @manga.save
+  end
+
+  test "should get manga list" do
+    get '/mangas'
+    assert_response :success
+  end
+
+  test "should get manga detail" do
+    get "/mangas/#{@manga.id}"
+    assert_response :success
+  end
 end
