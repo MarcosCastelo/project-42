35f7ee15a7a9769fc52744e558513bc16b2db18c
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-06-24 15:20:43 -0300

    feat: Create search function to get a list of mangas from url and params

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -5,4 +5,26 @@ class Scraper
   def initialize(base_url)
     @url = base_url
   end
+
+  def search(param)
+    html = URI.open(@url + param)
+    doc = Nokogiri::HTML(html)
+
+    mangas = []
+
+    doc.css(".manga-list-4-list li").each do |item|
+      manga = {
+        "title" => item.css('.manga-list-4-item-title a').first['title'],
+        "author" => item.css('.manga-list-4-item-tip a').first['title'],
+        "last_chapter" => item.css('.manga-list-4-item-tip a')[1].children.text,
+        "image" => item.css('.manga-list-4-cover')[0]['src'],
+        "status" => item.css('.manga-list-4-show-tag-list-2 a')[0].children.text,
+        "link" => @url + item.css('a')[0]['href']
+
+      }
+      mangas.append(manga)
+    end
+    
+    mangas
+  end
 end
