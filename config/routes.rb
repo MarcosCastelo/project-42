a2d6f53f21cdc51477ed06ae04a2b041b60bf552
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-06-30 19:26:42 -0300

    feat: create route to attach manga in user

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/mangas", to:"manga#index"
   get "/mangas/:id", to: "manga#show"
   get "/attach", to: "manga#attach", as: "attach_manga"
+  get "/add", to: "manga#add_manga_to_user", as: "add_manga_user"
 end
