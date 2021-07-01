8ba56e68cdb9c51f7965bc33865606b6c987cf30
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-06-30 22:59:44 -0300

    feat: change controller flow

--- app/controllers/manga_controller.rb
+++ app/controllers/manga_controller.rb
@@ -5,26 +5,27 @@ class MangaController < ApplicationController
 
   def show
     @manga = Manga.find(params[:id])
+
+    if params[:email]
+      @user = User.where(email: params[:email])[0]
+      if !@user
+        @user = User.create!(email: params[:email])
+      end
+
+      if !@manga.users.include?(@user)
+        @user.mangas << @manga
+        @user.save
+      end
+    end
+
   end
 
-  def attach
+  def add
     if params[:search]
       scraper = Scraper.new(params[:search])
-      @manga = scraper.save
-    end
-  end
-
-  def add_manga_to_user
-    user = User.where(params[:email])[0]
-    if !user
-      if User.where(params[:email]).empty?
-        user = User.new
-        user.email = params[:email]
-        user.mangas << @manga
-        user.save
-      else
-        user.mangas << @manga
-        user.save
+      @new_manga = scraper.save
+      if @new_manga 
+        redirect_to controller: 'manga', action: 'show', id: @new_manga.id
       end
     end
   end
