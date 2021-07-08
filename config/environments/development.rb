a70cbbacd0c7e72f803a6138669a7d7b90cc00d6
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-07-08 17:57:54 -0300

    feat: config sendiblue smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,15 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp-relay.sendinblue.com",
+    :port => 587,
+    :user_name => "marcosvitorcastelo@gmail.com",
+    :password => "gI0MUm29hcJZw5Gt",
+    :authentication => "login",
+    :enable_starttls_auto => true
+  }
 end
